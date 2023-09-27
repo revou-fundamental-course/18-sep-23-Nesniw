cb1e280404a7718239d41ec37ed4c6cebe40d474
winsen <koisurumetronome@gmail.com>
2023-09-28 03:44:19 +0700

    Submit Tugas Mini Coding Project

--- js/script.js
+++ js/script.js
@@ -5,4 +5,33 @@ setInterval(function(){
     if(counter > 4){
         counter = 1;
     }
-}, 5000);		
+}, 5000);
+
+function validateForm() {
+    const name = document.forms["message-form"]["your-name"].value;
+    const email = document.forms["message-form"]["email"].value;
+    const interest = document.forms["message-form"]["interest"].value;
+
+    if (name == "") {
+        alert("Nama tidak boleh kosong");
+        return false;
+    }
+
+    else if (email == ""){
+        alert("Email tidak boleh kosong");
+        return false;
+    }
+
+    else if (interest == "") {
+        alert("Interest tidak boleh kosong");
+        return false;
+    }
+
+    else {
+        alert("Terima kasih telah mengisi data");
+    }
+    
+    setSenderUI(name, birthDate, gender, messages);
+    
+    return false
+}
